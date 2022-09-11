2d891bc57635f0bb06137d4e623703f62aa61ca2
faraowait <arafabubakar4@gmail.com>
2022-09-11 03:56:10 -0700

    print_number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Printing numbers
+ *Return:0 success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
