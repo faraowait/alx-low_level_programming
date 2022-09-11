af289484474355d730b2d75c898b5cbf5310caf6
faraowait <arafabubakar4@gmail.com>
2022-09-11 03:33:06 -0700

    print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,17 +4,20 @@
  *main - print alphabets in lowercase except q and e
  *Return:0 success
  */
+
 int main(void)
 {
-char letters = 'a';
-while (letters <= 'z')
-{
-if ((letters != 'q') && (letters != 'e'))
-{	
-putchar(letters);
-}
-letters++;
-}
-putchar('\n');
-return (0);
+	char letters = 'a';
+
+	while (letters <= 'z')
+	{
+		if ((letters != 'q') && (letters != 'e'))
+		{
+			putchar(letters);
+		}
+		letters++;
+	}
+	putchar('\n');
+
+	return (0);
 }
