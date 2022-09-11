90018a904eb2a75c6859a6bb09c228e2160bc226
faraowait <arafabubakar4@gmail.com>
2022-09-11 08:52:15 -0700

    print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - prints hexadecimal base 0123456789abcdef
+ *Return:0 success
+ */
+
+int main(void)
+{
+	int n = '0';
+	int lett = 'a';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	while (lett <= 'f')
+	{
+		putchar(lett);
+		lett++;
+	}
+	putchar('\n');
+
+	return (0);
+}
