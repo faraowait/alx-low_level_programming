7be83a1dbf052b62b98009f97538887f1adf7c0b
faraowait <arafabubakar4@gmail.com>
2022-09-11 04:23:49 -0700

    print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabets in reverse
+ *Return:0 success
+ */
+
+int main(void)
+{
+	char alp = 'z';
+
+	while (alp >= 'a')
+	{
+		putchar(alp);
+		alp--;
+	}
+	putchar('\n');
+
+	return (0);
+}
