a80d22cb06adb9083e89c2cba2d158321c4fcae6
faraowait <arafabubakar4@gmail.com>
2022-09-16 14:49:34 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point of the function
+ * @n: Determines the length of the line
+ */
+
+void print_line(int c)
+{
+	int counter = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (counter++ < n)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
