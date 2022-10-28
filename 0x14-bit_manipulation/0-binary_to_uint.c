18672ce3b0bad4e3543a19363fa21dde1395a794
faraowait <arafabubakar4@gmail.com>
2022-10-27 22:33:01 -0700

    task_binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
  * binary_to_unit - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
- * Return: the converted numbrer
+ * Return: the converted number
  */
 unsigned int binary_to_unit(const char *b)
 {
